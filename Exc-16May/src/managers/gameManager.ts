06b08b0ac7df9829b2e53e798ecf3d21c2c46f65
nhatphong5954 <pcaonhat@gmail.com>
2025-05-19 00:03:26 +0700

    Create gameManager.ts

---
+++ Exc-16May/src/managers/gameManager.ts
@@ -0,0 +1,9 @@
+import { Button } from "../components/button.js";
+import { GameEngine } from "../engine/gameEngine.js";
+
+export class GameManager {
+    private engine: GameEngine;
+    private buttonList: Button[] = [];
+    private score: number = 0;
+
+}
